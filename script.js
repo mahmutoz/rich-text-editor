9b0cd5f977a221791d326e8e472a22ef431c6bc4
mahmutoz <mahmutoz8197@gmail.com>
2021-05-29 11:12:19 +0300

    All font families added and styles edited

--- script.js
+++ script.js
@@ -2,19 +2,10 @@
 
 const bodyEl = richTextField.document.getElementsByTagName('body')[0];
 let isSourceCode = false;
-let isEditMode = true;
-const btn = document.querySelectorAll('button');
 
 function editMode(){
     richTextField.document.designMode = 'On';
 }
-
-btn.forEach( e => {
-    e.addEventListener('click', function (){
-        e.classList.toggle('active');
-    });
-});
-
 
 function cmd(command){
     richTextField.document.execCommand(command, false, null);
